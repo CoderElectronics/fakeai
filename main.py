04710fdb4a959145f980ee1fbce2494a846ba572
Brogan <bmutarellI@ideaventionsacademy.org>
2024-04-17 14:10:35 -0400

    commit

--- main.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import tarfile
-from sklearn.preprocessing import LabelEncoder
-from sklearn.model_selection import train_test_split
-
-#load data
-all_data = pd.read_csv('data/allData.tsv', sep='\t')
-
-train_raw = all_data[all_data.text.not_null()]
