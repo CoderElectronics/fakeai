93ac4ba238d5238902274874ac19843532588cad
CoderElectronics <aristehney@gmail.com>
2024-05-09 15:42:01 -0400

    Add fake-and-real-news dataset downloading

--- download_datasets.py
+++ download_datasets.py
@@ -20,3 +20,6 @@ Path("data/orig").mkdir(parents=True, exist_ok=True)
 
 urllib.request.urlretrieve("https://zenodo.org/record/4561253/files/WELFake_Dataset.csv?download=1", "data/orig/WELFake_Dataset.csv", lambda *x: show_progress(*x, msg="Downloading WELFake..."))
 urllib.request.urlretrieve("https://dataset-dl-fakeai.surge.sh/FakeNewsNet.csv", "data/orig/FakeNewsNet.csv", lambda *x: show_progress(*x, msg="Downloading FakeNewsNet..."))
+
+urllib.request.urlretrieve("https://fake-and-real-news-dataset.surge.sh/True.csv", "data/orig/True.csv", lambda *x: show_progress(*x, msg="Downloading True-or-Fake-dataset (true)..."))
+urllib.request.urlretrieve("https://fake-and-real-news-dataset.surge.sh/Fake.csv", "data/orig/Fake.csv", lambda *x: show_progress(*x, msg="Downloading True-or-Fake-dataset (fake)..."))
